7804f48b193ff955000425c22f03c42741f626a5
LapitskayaN <lapitskaya.nl@gmail.com>
2021-01-21 02:06:07 +0300

    yell

--- exercises/yell.js
+++ exercises/yell.js
@@ -7,9 +7,15 @@
  */
 
 // Your code:
-
+const yell = (str) =>{
+    let res = str.toUpperCase()
+    return res;
+ }
+ 
 //* Begin of tests
 const assert = require('assert');
-
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof yell, 'function');
+assert.deepStrictEqual(yell('abc'),('ABC'));
+assert.deepStrictEqual(yell('Cat'),('CAT'));
+//assert.fail('You must write your own tests');
 // End of tests */
