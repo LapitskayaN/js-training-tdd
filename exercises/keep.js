3ed5956baf26bb7931f77dec20db2d86c7767905
LapitskayaN <lapitskaya.nl@gmail.com>
2021-01-21 02:55:50 +0300

    keep

--- exercises/keep.js
+++ exercises/keep.js
@@ -13,9 +13,41 @@
  */
 
 // Your code:
-
+const keepFirst = (str) =>{
+    let arr=str.split('');
+    let res ='';
+    return res=arr[0]+arr[1];
+ }
+ const keepLast = (str) =>{
+    let arr=str.split('');
+    let res ='';
+    return res=arr[arr.length-2]+arr[arr.length-1];
+ }
+ const keepFirstLast = (str) =>{
+    let arr=str.split('');
+    let res ='';
+    if (arr.length>=2) {
+            for (let i=2; i<arr.length; i++) {
+                res +=arr[i];
+            }
+    }  else {
+        res =res+'';
+    }
+    return res;
+ }
 //* Begin of tests
 const assert = require('assert');
+assert.strictEqual(typeof keepFirst, 'function');
+assert.deepStrictEqual(keepFirst('ABC'),('AB'));
+assert.deepStrictEqual(keepFirst('Catsss'),('Ca'));
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof keepLast, 'function');
+assert.deepStrictEqual(keepLast('ABC'),('BC'));
+assert.deepStrictEqual(keepLast('Catsss'),('ss'));
+
+assert.strictEqual(typeof keepFirstLast, 'function');
+assert.deepStrictEqual(keepFirstLast('ABC'),('C'));
+assert.deepStrictEqual(keepFirstLast('Catsss'),('tsss'));
+assert.deepStrictEqual(keepFirstLast('Catdog'),('tdog'));
+//assert.fail('You must write your own tests');
 // End of tests */
