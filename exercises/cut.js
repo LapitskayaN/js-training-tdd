041e4126b1373aca93148274f576cb96f7090b11
LapitskayaN <lapitskaya.nl@gmail.com>
2021-01-21 02:43:12 +0300

    cut

--- exercises/cut.js
+++ exercises/cut.js
@@ -11,9 +11,49 @@
  */
 
 // Your code:
+const cutFirst = (str) =>{
+    let arr=str.split('');
+    let res ='';
+    for (let i=2; i<arr.length; i++) {
+        res +=arr[i];
+    }
+    return res;
+ }
+ const cutLast = (str) =>{
+    let arr=str.split('');
+    let res ='';
+    for (let i=0; i<arr.length-2; i++) {
+        res +=arr[i];
+    }
+    return res;
+ }
+ const cutFirstLast = (str) =>{
+    let arr=str.split('');
+    let res ='';
+    if (arr.length>=4) {
+            for (let i=2; i<arr.length-2; i++) {
+                res +=arr[i];
+            }
+    }  else {
+        res =res+'';
+    }
+    return res;
+ }
 
 //* Begin of tests
 const assert = require('assert');
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof cutFirst, 'function');
+assert.deepStrictEqual(cutFirst('ABC'),('C'));
+assert.deepStrictEqual(cutFirst('Catsss'),('tsss'));
+
+assert.strictEqual(typeof cutLast, 'function');
+assert.deepStrictEqual(cutLast('ABC'),('A'));
+assert.deepStrictEqual(cutLast('Catsss'),('Cats'));
+
+assert.strictEqual(typeof cutFirstLast, 'function');
+assert.deepStrictEqual(cutFirstLast('ABC'),(''));
+assert.deepStrictEqual(cutFirstLast('Catsss'),('ts'));
+assert.deepStrictEqual(cutFirstLast('Catdog'),('td'));
+//assert.fail('You must write your own tests');
 // End of tests */
