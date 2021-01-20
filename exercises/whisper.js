ebdb8383429edc4e1f0f03f8b0909bfd7588ef63
LapitskayaN <lapitskaya.nl@gmail.com>
2021-01-21 02:09:46 +0300

    whisper

--- exercises/whisper.js
+++ exercises/whisper.js
@@ -8,9 +8,15 @@
  */
 
 // Your code:
-
+const whisper = (str) =>{
+    let res = str.toLowerCase()
+    return res;
+ }
+ 
 //* Begin of tests
 const assert = require('assert');
-
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof whisper, 'function');
+assert.deepStrictEqual(whisper('ABC'),('abc'));
+assert.deepStrictEqual(whisper('Cat'),('cat'));
+//assert.fail('You must write your own tests');
 // End of tests */
